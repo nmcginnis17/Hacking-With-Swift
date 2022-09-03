4a287a775068a442155e1236891165893a12fa74
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-03 08:08:38 -0400

    Added image view

--- Storm Viewer/Storm Viewer/DetailViewController.swift
+++ Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -1,35 +1,31 @@
 //
 //  DetailViewController.swift
-//  Project1
+//  Storm Viewer
 //
-//  Created by TwoStraws on 12/08/2016.
-//  Copyright © 2016 Paul Hudson. All rights reserved.
+//  Created by Nicholas McGinnis on 9/3/22.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
-	@IBOutlet var imageView: UIImageView!
-	var selectedImage: String?
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		title = selectedImage
-        navigationItem.largeTitleDisplayMode = .never
-
-		if let imageToLoad = selectedImage {
-			imageView.image  = UIImage(named: imageToLoad)
-		}
+        // Do any additional setup after loading the view.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.hidesBarsOnTap = true
-    }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.hidesBarsOnTap = false
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
