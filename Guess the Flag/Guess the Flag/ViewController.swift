67308657a96aa29e9f5cdd493647700290d822dc
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-05 11:23:37 -0400

    Guess the Flag Complete

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var correctAnswer = 0
+    var questionsAsked = 0
     var score = 0
 
     override func viewDidLoad() {
@@ -34,15 +35,46 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        questionsAsked += 1
+        updateTitle()
+    }
+    
+    func restart(action: UIAlertAction) {
+        questionsAsked = 0
+        score = 0
+        askQuestion()
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+            if sender.tag == correctAnswer {
+                title = "Correct"
+                score += 1
+            } else {
+                title = "Wrong! That flag belongs to \(countries[sender.tag].uppercased())"
+//                score -= 1
+            }
+        
+            if questionsAsked == 10 {
+                let ac1 = UIAlertController(title: "Your score is \(score)/\(questionsAsked)", message: "Would you like to restart?", preferredStyle: .alert)
+                print("Your score is \(score)/\(questionsAsked)")
+                ac1.addAction(UIAlertAction(title: "Restart", style: .default, handler: restart))
+                present(ac1, animated: true)
+            } else {
+                let ac2 = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+                ac2.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+                present(ac2, animated:  true)
+            }
+    }
+    
+    func updateTitle() {
+        title = "\(countries[correctAnswer].uppercased()) - Score: \(score)/\(questionsAsked)"
+    }
+    
 }
 
