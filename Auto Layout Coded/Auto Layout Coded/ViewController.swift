f6c8e167b7b45c1655bf16de854183b821f25007
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-11 21:07:40 -0400

    Auto Layout Complete

--- Auto Layout Coded/Auto Layout Coded/ViewController.swift
+++ Auto Layout Coded/Auto Layout Coded/ViewController.swift
@@ -16,30 +16,40 @@ class ViewController: UIViewController {
         label1.translatesAutoresizingMaskIntoConstraints = false
         label1.backgroundColor = UIColor.red
         label1.text = "THESE"
+        label1.textAlignment = .center
+        label1.textColor = UIColor.lightGray
         label1.sizeToFit()
         
         let label2 = UILabel()
         label2.translatesAutoresizingMaskIntoConstraints = false
         label2.backgroundColor = UIColor.cyan
         label2.text = "ARE"
+        label2.textAlignment = .center
+        label2.textColor = UIColor.brown
         label2.sizeToFit()
         
         let label3 = UILabel()
         label3.translatesAutoresizingMaskIntoConstraints = false
         label3.backgroundColor = UIColor.yellow
         label3.text = "SOME"
+        label3.textAlignment = .center
+        label3.textColor = UIColor.black
         label3.sizeToFit()
         
         let label4 = UILabel()
         label4.translatesAutoresizingMaskIntoConstraints = false
         label4.backgroundColor = UIColor.green
         label4.text = "AWESOME"
+        label4.textAlignment = .center
+        label4.textColor = UIColor.darkGray
         label4.sizeToFit()
         
         let label5 = UILabel()
         label5.translatesAutoresizingMaskIntoConstraints = false
         label5.backgroundColor = UIColor.orange
         label5.text = "LABELS"
+        label5.textAlignment = .center
+        label5.textColor = UIColor.white
         label5.sizeToFit()
         
         view.addSubview(label1)
@@ -47,6 +57,10 @@ class ViewController: UIViewController {
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
+        
+        var previous: UILabel?
+        
+        let metrics = ["labelHeight": 88]
         
         let viewsDictionary = ["label1": label1,
                                "label2": label2,
@@ -62,11 +76,30 @@ class ViewController: UIViewController {
         
 //        SHORT HAND WAY TO DO THE ABOVE
 //        USE STRING INTERPOLATION TO PUT THE KEY INTO THE VFL
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        ASSIGN VERTICAL CONSTRAINTS
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                // we have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // this is the first label
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
         
     }
 
