25deb8c1c733c25ca9365239da5c33350e0d56c2
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-12 23:02:52 -0400

    Whitehouse Petitions

---
+++ Whitehouse Petitions/Whitehouse Petitions/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Whitehouse Petitions
+//
+//  Created by Nicholas McGinnis on 9/12/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Whitehouse Petitions/Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Whitehouse Petitions
+//
+//  Created by Nicholas McGinnis on 9/12/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
