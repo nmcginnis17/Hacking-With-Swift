5c3544f9b3da294921d15df33f91e9809f90413f
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-16 22:53:14 -0400

    Swifty Words Initial Commit

---
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Nicholas McGinnis on 9/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
