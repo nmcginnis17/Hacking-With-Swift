30353a5f70430c3f45f13d52f4bcf45a1fa64945
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-09-27 23:15:55 -0400

    Collection View, added btn to import photos. PHOTOS DO NOT SHOW AT THIS POINT

---
+++ Names to Faces/Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Nicholas McGinnis on 9/27/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
