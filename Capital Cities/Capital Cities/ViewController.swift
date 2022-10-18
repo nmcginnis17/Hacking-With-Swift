b8ad2bde0b654d5cb9f7d32672dad9bcb2698e04
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-10-17 22:45:04 -0400

    Capital Cities Initial Commit

---
+++ Capital Cities/Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by Nicholas McGinnis on 10/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
