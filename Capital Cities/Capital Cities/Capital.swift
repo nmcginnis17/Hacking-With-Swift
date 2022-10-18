36eef2822e35e1311479393c82e0fe2941499069
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-10-17 23:38:37 -0400

    View annotations on Map

---
+++ Capital Cities/Capital Cities/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Nicholas McGinnis on 10/17/22.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+}
