6618c10b4fafc91b239f751e0d8475be0223812b
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-10-19 23:57:37 -0400

    JavaScript Injection

---
+++ JavaScript Injection/JavaScript Injection Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,24 @@
+//
+//  SafariWebExtensionHandler.swift
+//  JavaScript Injection Extension
+//
+//  Created by Nicholas McGinnis on 10/19/22.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
---
+++ JavaScript Injection/JavaScript Injection/JavaScript Injection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaScript Injection
+//
+//  Created by Nicholas McGinnis on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
