e0732178d148b298b25722a9854f74a5e3e77d70
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-10-25 22:49:42 -0400

    Local Notifications initial commit

---
+++ Local Notifications/Local Notifications/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Local Notifications
+//
+//  Created by Nicholas McGinnis on 10/24/22.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
+
+
+}
+
