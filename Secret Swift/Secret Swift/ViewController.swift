60c130d7c9cb0a7716c7a088e8202fd73e317cee
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-12-29 22:58:07 -0500

    Secret Swift initial commit

---
+++ Secret Swift/Secret Swift/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Secret Swift
+//
+//  Created by Nicholas McGinnis on 12/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var secret: UITextView!
+    @IBOutlet weak var authBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        secret.isHidden = true
+        authBtn.setTitle("Authenticate", for: .normal)
+        authBtn.heightAnchor.constraint(equalToConstant: 44).isActive = true
+    }
+    
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+
+    @IBAction func authenticateTapped(_ sender: Any) {
+        
+    }
+    
+}
+
