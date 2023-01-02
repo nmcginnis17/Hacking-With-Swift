8386c2a1088ef888aec2d6153dccc788610a7edc
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-01 23:17:30 -0500

    add items to stackView

--- Multibrowser/Multibrowser/ViewController.swift
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var addressBar: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
+    }
+    
+    func setDefaultTitle() {
+        title = "Multibrowser"
     }
 
+    @objc func addWebView() {
+        // Add new WKWebView to UIStackView
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    @objc func deleteWebView() {
+        
+    }
 
 }
 
