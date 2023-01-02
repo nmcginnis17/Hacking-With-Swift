14549c1728154dd1620b531cd413a53fb42434a0
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-01 22:43:40 -0500

    Multibrowser initial commit

---
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Nicholas McGinnis on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
