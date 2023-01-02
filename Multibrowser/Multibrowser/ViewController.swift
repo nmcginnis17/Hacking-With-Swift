701f3b159851811f0f0a2bd9ce6cecbd56a2eb2a
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-01 23:37:41 -0500

    enter url for each new stackView item, gestureRecognizer to selected views

--- Multibrowser/Multibrowser/ViewController.swift
+++ Multibrowser/Multibrowser/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var addressBar: UITextField!
     
+    weak var activeWebView: WKWebView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,10 +38,47 @@ class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegat
         
         let url = URL(string: "https://hackingwithswift.com")!
         webView.load(URLRequest(url: url))
+        
+        webView.layer.borderColor = UIColor.blue.cgColor
+        selectWebView(webView)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
     }
     
     @objc func deleteWebView() {
         
+    }
+    
+    @objc func webViewTapped(_ recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? WKWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func selectWebView(_ webView: WKWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let webView = activeWebView, let address = addressBar.text {
+            if let url = URL(string: "https://\(address)") {
+                webView.load(URLRequest(url: url))
+            }
+        }
+        
+        textField.resignFirstResponder()
+        return true
     }
 
 }
