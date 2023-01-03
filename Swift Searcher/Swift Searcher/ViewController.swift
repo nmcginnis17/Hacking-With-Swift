05b7256e2838b4f399b45ce3b23a71cfae42587c
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-03 00:33:35 -0500

    Swift Searcher Initial Commit

---
+++ Swift Searcher/Swift Searcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Searcher
+//
+//  Created by Nicholas McGinnis on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
