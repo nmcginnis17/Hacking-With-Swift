fdc0ec7a94d2e5edef55bb184fdd4bd38db0dadc
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-05 23:10:04 -0500

    What's that Tune Initial Commit

---
+++ What's that tune/What's that tune/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  What's that tune
+//
+//  Created by Nicholas McGinnis on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "What's that Tune?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTune))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+    }
+    
+    
+    @objc func addTune() {
+        let vc = RecordTuneViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
