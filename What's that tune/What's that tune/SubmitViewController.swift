f5fdd916d2ee79a6fed2a037f5a31cc45be0c800
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-08 21:43:57 -0500

    Set up view for SubmitViewController

---
+++ What's that tune/What's that tune/SubmitViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SubmitViewController.swift
+//  What's that tune
+//
+//  Created by Nicholas McGinnis on 1/8/23.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+    
+    var genre: String!
+    var comments: String!
+    
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "You're all set!"
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = UIColor.white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(style: .large)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+    
+    func doSubmission() {
+        
+    }
+    
+    @objc func doneTapped() {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    
+}
