d2cabadfd2e957dbd4e2be104a62fdb6daadbb10
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-08 22:22:08 -0500

    Introduction to CloudKit

--- What's that tune/What's that tune/SubmitViewController.swift
+++ What's that tune/What's that tune/SubmitViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Nicholas McGinnis on 1/8/23.
 //
 
+import CloudKit
 import UIKit
 
 class SubmitViewController: UIViewController {
@@ -65,7 +66,30 @@ class SubmitViewController: UIViewController {
     }
     
     func doSubmission() {
+        let tuneRecord = CKRecord(recordType: "Tunes")
+        tuneRecord["genre"] = genre as CKRecordValue
+        tuneRecord["comments"] = comments as CKRecordValue
         
+        let audioURL = RecordTuneViewController.getTuneURL()
+        let tuneAsset = CKAsset(fileURL: audioURL)
+        tuneRecord["audio"] = tuneAsset
+        
+        CKContainer.default().publicCloudDatabase.save(tuneRecord) { [unowned self] record, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.status.text = "Error:\(error.localizedDescription)"
+                    self.spinner.stopAnimating()
+                } else {
+                    self.view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+                    self.status.text = "Done!"
+                    self.spinner.stopAnimating()
+                    
+                    ViewController.isDirty()
+                }
+                
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneTapped))
+            }
+        }
     }
     
     @objc func doneTapped() {
