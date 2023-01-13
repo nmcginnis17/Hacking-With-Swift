4c32933308ab0af246081aa3b90feb6dbbc6800a
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-12 23:31:52 -0500

    Created board columns, clear board func.

---
+++ Four in a Row/Four in a Row/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  Four in a Row
+//
+//  Created by Nicholas McGinnis on 1/12/23.
+//  Model
+
+import UIKit
+
+class Board: NSObject {
+    static var width = 7
+    static var height = 6
+}
--- Four in a Row/Four in a Row/ViewController.swift
+++ Four in a Row/Four in a Row/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var placedChips = [[UIView]]()
+    var board: Board!
+    
+    @IBOutlet var columnButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for _ in 0 ..< Board.width {
+            placedChips.append([UIView]())
+        }
+        resetBoard()
     }
+    
+    @IBAction func makeMove(_ sender: UIButton) {
+        
+    }
+    
+    func resetBoard() {
+        board = Board()
+        
+        for i in 0 ..< placedChips.count {
+            for chip in placedChips[i] {
+                chip.removeFromSuperview()
+            }
+            placedChips[i].removeAll(keepingCapacity: true)
+        }
+    }
+    
 
 
 }
