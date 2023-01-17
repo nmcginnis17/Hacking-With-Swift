30051d932f7ef144a493743afff935c8c77c2d21
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-16 23:43:02 -0500

    Player displays on screen

--- Crashy Plane/Crashy Plane/GameScene.swift
+++ Crashy Plane/Crashy Plane/GameScene.swift
@@ -10,12 +10,30 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
-        
+        createPlayer()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+    }
+    
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "player-1")
+        player = SKSpriteNode(texture: playerTexture)
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
+        
+        addChild(player)
+        
+        let frame2 = SKTexture(imageNamed: "player-2")
+        let frame3 = SKTexture(imageNamed: "player-3")
+        let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
+        let runForever = SKAction.repeatForever(animation)
+        
+        player.run(runForever)
     }
     
 }
