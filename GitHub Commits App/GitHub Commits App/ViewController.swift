05ff06d22adc1f1fff84eb6dcf59289489cdd192
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-21 00:14:11 -0500

    GitHub Commits App Initial Commit

---
+++ GitHub Commits App/GitHub Commits App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Commits App
+//
+//  Created by Nicholas McGinnis on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
