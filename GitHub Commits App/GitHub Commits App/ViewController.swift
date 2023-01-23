36be67dba5350930af342de11851d493db17b3d0
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-22 23:34:50 -0500

    Added NSPersistentContainer & save function for CoreData

--- GitHub Commits App/GitHub Commits App/ViewController.swift
+++ GitHub Commits App/GitHub Commits App/ViewController.swift
@@ -5,13 +5,32 @@
 //  Created by Nicholas McGinnis on 1/20/23.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var container: NSPersistentContainer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        container = NSPersistentContainer(name: "GitHub Commits App")
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print("Unresolved error \(error)")
+            }
+        }
+    }
+    
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            }
+        }
     }
 
 
