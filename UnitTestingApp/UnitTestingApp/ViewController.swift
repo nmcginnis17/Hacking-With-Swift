f0326838c472323e482986952bebb4c0f3ce4f3b
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-01-28 22:31:25 -0500

    UnitTestApp Practice initial commit

---
+++ UnitTestingApp/UnitTestingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestingApp
+//
+//  Created by Nicholas McGinnis on 1/28/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
