8c8368babb267e9c0495f3789889fe890f37f58c
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-06-13 23:33:30 -0400

    Permission authorization requests added.

--- Advanced-iOS-1/Happy Days/Happy Days/MemoriesViewController.swift
+++ Advanced-iOS-1/Happy Days/Happy Days/MemoriesViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Nicholas McGinnis on 6/13/23.
 //
 
+import AVFoundation
+import Photos
+import Speech
 import UIKit
 
 class MemoriesViewController: UICollectionViewController {
@@ -12,7 +15,21 @@ class MemoriesViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        checkPermissions()
+    }
+    
+    func checkPermissions() {
+        let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
+        let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission == .granted
+        let transcribeAutorized = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        let authorized = photosAuthorized && recordingAuthorized && transcribeAutorized
+        
+        if authorized == false {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "FirstRun") {
+                navigationController?.present(vc, animated: true)
+            }
+        }
     }
 
 }
--- Advanced-iOS-1/Happy Days/Happy Days/ViewController.swift
+++ Advanced-iOS-1/Happy Days/Happy Days/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Nicholas McGinnis on 6/13/23.
 //
 
+import AVFoundation
+import Photos
+import Speech
 import UIKit
 
 class ViewController: UIViewController {
@@ -18,7 +21,47 @@ class ViewController: UIViewController {
     }
 
     @IBAction func requestPermissions(_ sender: Any) {
-        
+        requestPhotosPermissions()
+    }
+    
+    func requestPhotosPermissions() {
+        PHPhotoLibrary.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.requestRecordPermissions()
+                } else {
+                    self.helpLabel.text = "Photos permission was declined; please enable it in settings the tap continue."
+                }
+            }
+        }
+    }
+    
+    func requestRecordPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission() { [unowned self] allowed in
+            DispatchQueue.main.async {
+                if allowed {
+                    self.requestTranscribePermissions()
+                } else {
+                    self.helpLabel.text = "Recording permission was declined; please enable it in settings the tap continue."
+                }
+            }
+        }
+    }
+    
+    func requestTranscribePermissions() {
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.authorizationComplete()
+                } else {
+                    self.helpLabel.text = "Transcription permission was declined; please enable it in settings the tap continue."
+                }
+            }
+        }
+    }
+    
+    func authorizationComplete() {
+        dismiss(animated: true)
     }
     
 }
