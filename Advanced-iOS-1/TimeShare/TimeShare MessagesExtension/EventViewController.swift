077be20e54dd4b5c8b92cff7a430e3602a346f28
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-07-01 22:11:21 -0400

    Add dates to arrays, animate tableView

--- Advanced-iOS-1/TimeShare/TimeShare MessagesExtension/EventViewController.swift
+++ Advanced-iOS-1/TimeShare/TimeShare MessagesExtension/EventViewController.swift
@@ -12,6 +12,10 @@ class EventViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet var tableView: UITableView!
     @IBOutlet var datePicker: UIDatePicker!
     
+    var dates = [Date]()
+    var allVotes = [Int]()
+    var ourVotes = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +27,7 @@ class EventViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dates.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,7 +37,20 @@ class EventViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     @IBAction func addDate(_ sender: AnyObject) {
-       
+       // add to arrays
+        dates.append(datePicker.date)
+        allVotes.append(0)
+        ourVotes.append(1)
+        
+        // insert new row in the table
+        let newIndexPath = IndexPath(row: dates.count - 1, section: 0)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        
+        // scroll the new row into view
+        tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+        
+        // flash the scroll bars so the user knows something changed
+        tableView.flashScrollIndicators()
     }
 
     @IBAction func saveSelectedDates(_ sender: AnyObject) {
