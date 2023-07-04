ba147a33d9ba28fbfb4d05ee95da5e918c5dc518
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-07-03 21:46:22 -0400

    Rideshare method stubs updated

--- Advanced-iOS-1/ChooseCruise/Extension/IntentHandler.swift
+++ Advanced-iOS-1/ChooseCruise/Extension/IntentHandler.swift
@@ -8,6 +8,51 @@
 import Intents
 import UIKit
 
-class IntentHandler: INExtension {
+class IntentHandler: INExtension, INRidesharingDomainHandling {
+    func handle(intent: INListRideOptionsIntent, completion: @escaping (INListRideOptionsIntentResponse) -> Void) {
+        // list of rides available
+    }
+    
+    func handle(intent: INRequestRideIntent, completion: @escaping (INRequestRideIntentResponse) -> Void) {
+        // ride has been requested using Maps or Siri
+    }
+    
+    func handle(intent: INGetRideStatusIntent, completion: @escaping (INGetRideStatusIntentResponse) -> Void) {
+        // get ride status
+        /// called when user asks where their ride is, connect to your server to get current location of ride and send it back
+        let result = INGetRideStatusIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+    
+    func startSendingUpdates(for intent: INGetRideStatusIntent, to observer: INGetRideStatusIntentResponseObserver) {
+        //
+    }
+    
+    func stopSendingUpdates(for intent: INGetRideStatusIntent) {
+        //
+    }
+    
+    func handle(cancelRide intent: INCancelRideIntent, completion: @escaping (INCancelRideIntentResponse) -> Void) {
+        // cancel ride request
+        /// connect to your server
+        let result = INCancelRideIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+    
+    func handle(sendRideFeedback sendRideFeedbackintent: INSendRideFeedbackIntent, completion: @escaping (INSendRideFeedbackIntentResponse) -> Void) {
+        // leave review for driver
+        /// connect to your server
+        let result = INSendRideFeedbackIntentResponse(code: .success, userActivity: nil)
+        completion(result)
+    }
+    
+    func resolvePickupLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        //
+    }
+    
+    func resolveDropOffLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        //
+    }
+    
     
 }
