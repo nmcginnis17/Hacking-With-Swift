250e8dc1467de369148792075548b2e5300bb0fc
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-08-26 23:22:28 -0400

    Added slider, and animate redBox

--- Advanced-iOS-1/Techniques/Techniques/ViewController.swift
+++ Advanced-iOS-1/Techniques/Techniques/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var animator: UIViewPropertyAnimator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.addTarget(self, action: #selector(sliderChanged), for: .valueChanged)
+        view.addSubview(slider)
+        
+        slider.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10).isActive = true
+        slider.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        
+        let redBox = UIView(frame: CGRect(x: -64, y: 0, width: 128, height: 128))
+        redBox.translatesAutoresizingMaskIntoConstraints = false
+        redBox.backgroundColor = UIColor.red
+        redBox.center.y = view.center.y
+        view.addSubview(redBox)
+        
+        animator = UIViewPropertyAnimator(duration: 2, curve: .easeInOut) { [unowned self, redBox] in
+            redBox.center.x = self.view.frame.width
+            redBox.transform = CGAffineTransform(rotationAngle: CGFloat.pi).scaledBy(x: 0.001, y: 0.001)
+        }
+    }
+    
+    @objc func sliderChanged(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
     }
 
 
