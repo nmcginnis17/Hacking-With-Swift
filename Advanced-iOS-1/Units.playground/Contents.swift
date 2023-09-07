23aa03d4cd6d54a0848530987a04e7046f9b4199
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2023-09-06 23:44:36 -0400

    Unit conversions

--- Advanced-iOS-1/Units.playground/Contents.swift
+++ Advanced-iOS-1/Units.playground/Contents.swift
@@ -19,3 +19,16 @@ let squareCentimeters = squareMeters.converted(to: .squareCentimeters)
 //convert bushels to imperial teaspoons
 let bushels = Measurement(value: 6, unit: UnitVolume.bushels)
 let teaspoons = bushels.converted(to: .imperialTeaspoons)
+
+//calculations
+let doubleheight = heightFeet * 2
+let firstLap = Measurement(value: 49, unit: UnitDuration.seconds)
+let secondLap = Measurement(value: 58, unit: UnitDuration.seconds)
+let thirdLap = Measurement(value: 1.3, unit: UnitDuration.minutes)
+let total = (firstLap + secondLap + thirdLap).converted(to: .minutes)
+
+let formatter = MeasurementFormatter()
+let temperature = Measurement(value: 32, unit: UnitTemperature.celsius)
+formatter.unitStyle = .long
+let result = formatter.string(from: temperature)
+formatter.unitOptions = .providedUnit
