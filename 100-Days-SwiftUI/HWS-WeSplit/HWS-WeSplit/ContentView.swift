eccf01fb79dd8b76fc1f32e21a16e07a9a3968d4
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-02-20 23:15:46 -0500

    Start of 100-Days-of-SwiftUI

---
+++ 100-Days-SwiftUI/HWS-WeSplit/HWS-WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HWS-WeSplit
+//
+//  Created by Nicholas McGinnis on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100-Days-SwiftUI/HWS-WeSplit/HWS-WeSplit/HWS_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_WeSplitApp.swift
+//  HWS-WeSplit
+//
+//  Created by Nicholas McGinnis on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HWS_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
